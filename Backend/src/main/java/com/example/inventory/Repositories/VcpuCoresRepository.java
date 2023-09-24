c4e864c847f6af434042af8f11ec6bd747b72dbf
Mostafa Arafa <>
2023-09-25 02:29:58 +0300

    Update Repositories

--- Backend/src/main/java/com/example/inventory/Repositories/VcpuCoresRepository.java
+++ Backend/src/main/java/com/example/inventory/Repositories/VcpuCoresRepository.java
@@ -1,9 +1,11 @@
 package com.example.inventory.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.inventory.Data.VcpuCores;
 
+@Repository
 public interface VcpuCoresRepository extends JpaRepository<VcpuCores,Integer> {
     
 }
