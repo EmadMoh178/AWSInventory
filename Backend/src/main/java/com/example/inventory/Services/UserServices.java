34e5aff69629e39b4d37d2580eeb3f8c3deb216c
Omar Ashour <omarashour@Omar-MacBook-Pro.local>
2023-09-25 19:45:44 +0300

    Added Security Configuration

--- Backend/src/main/java/com/example/inventory/Services/UserServices.java
+++ Backend/src/main/java/com/example/inventory/Services/UserServices.java
@@ -4,13 +4,17 @@ package com.example.inventory.Services;
 import com.example.inventory.Data.Users;
 import com.example.inventory.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserServices {
+public class UserServices implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
@@ -21,14 +25,20 @@ public class UserServices {
         return userRepository.findAll();
     }
 
-    public Users login(Users user){
+    public Users login(Users user) throws UsernameNotFoundException {
         int id = user.getId();
-        String password = user.getPassword();
         Optional<Users> usersOptional = userRepository.findById(id);
-        Users loginUser = new Users();
-        if(usersOptional.isPresent())
-            loginUser = usersOptional.get();
+        Users loginUser;
+        if(usersOptional.isEmpty())
+            throw new UsernameNotFoundException("User with ID" + id + " is not found");
+        loginUser = usersOptional.get();
         return loginUser;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), new ArrayList<>());
     }
 
 }
