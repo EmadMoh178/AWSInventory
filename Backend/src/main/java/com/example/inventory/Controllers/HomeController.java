f6904c823349ceb8de2b223ecb9e0e9d64a98d61
Omar Ashour <omarashour@Omar-MacBook-Pro.local>
2023-09-27 15:16:24 +0300

    Connected to Aurora RDS through application properties

--- Backend/src/main/java/com/example/inventory/Controllers/HomeController.java
+++ Backend/src/main/java/com/example/inventory/Controllers/HomeController.java
@@ -1,16 +1,23 @@
 package com.example.inventory.Controllers;
 
+import com.example.inventory.Data.Ec2Instances;
+import com.example.inventory.Repositories.Ec2Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 
 public class HomeController {
 
+    @Autowired
+    Ec2Repository ec2Repository;
     @GetMapping("/")
-    public String sayHello() {
-        return "Hello world";
+    public List<Ec2Instances> sayHello() {
+        return ec2Repository.findAll();
     }
 
 }
