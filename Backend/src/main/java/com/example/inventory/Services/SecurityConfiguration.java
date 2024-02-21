57c793bb71a841e3f31bab220776acc242eb238c
Omar Ashour <68746440+OmarAshour02@users.noreply.github.com>
2024-02-21 17:57:16 +0200

    Update SecurityConfiguration.java

--- Backend/src/main/java/com/example/inventory/Services/SecurityConfiguration.java
+++ Backend/src/main/java/com/example/inventory/Services/SecurityConfiguration.java
@@ -1,45 +1,45 @@
-// package com.example.inventory.Services;
+package com.example.inventory.Services;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.authentication.AuthenticationProvider;
-// import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-// import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.Customizer.withDefaults;
 
-// @Configuration
-// public class SecurityConfiguration {
+@Configuration
+public class SecurityConfiguration {
 
-//     @Autowired
-//     private UserServices userDetailsService;
+    @Autowired
+    private UserServices userDetailsService;
 
-//     @Bean
-//     public PasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-//     @Bean
-//     public AuthenticationProvider authenticationProvider() {
-//         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-//         provider.setUserDetailsService(userDetailsService);
-//         provider.setPasswordEncoder(passwordEncoder());
-//         return provider;
-//     }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
 
-//     @Bean
-//     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//         http
-//                 .authorizeHttpRequests(authorizeRequests ->
-//                         authorizeRequests
-//                                 .requestMatchers("/signup").permitAll()
-//                                 .anyRequest().authenticated()
-//                 )
-//                 .formLogin(withDefaults());
-//         return http.build();
-//     }
-// }
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/signup").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+        return http.build();
+    }
+}
